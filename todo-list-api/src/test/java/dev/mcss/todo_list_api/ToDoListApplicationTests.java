c35353979154e7e825b16d23d0936f28dcd9ab5f
Maila <cardmaila19@gmail.com>
2025-11-23 16:12:14 -0300

    Iniciando backend do todo

---
+++ todo-list-api/src/test/java/dev/mcss/todo_list_api/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mcss.todo_list_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
